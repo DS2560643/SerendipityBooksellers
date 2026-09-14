2f27d69bd13252a9302ae467f205b41fb61c1626
DS2560643 <dev.2560643@nv.ccsd.net>
2026-09-14 21:01:02 +0000

    Committing 9/14

---
+++ Part6/bookinfo.cpp
@@ -0,0 +1,11 @@
+#include "bookinfo.h"
+#include <iostream>
+using namespace std;
+
+void bookinfo()
+{
+    cout << " Serendipity Booksellers\n";
+	cout << " Book Information\n\n";
+	
+	cout << "ISBN:\nTitle:\nAuthor:\nPublisher:\nDate Added:\nQuantity-On-Hand:\nWholesale Cost:\nRetail Price:";
+}
--- Part6/mainmenu.cpp
+++ Part6/mainmenu.cpp
@@ -1,7 +1,45 @@
-#include<iostream>
+#include "bookinfo.h"
+#include "cashier.h"
+#include "invmenu.h"
+#include "reports.h"
+
+#include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello World" << endl;
-    return 0;
+int main()
+{
+	cout << " Serendipity Booksellers" << endl;
+	cout << "\tMain Menu\n" << endl;
+	cout << "1.  Cashier Module\n2.  Inventory Dashboard Module\n3.  Report Module\n4.  Exit\n" << endl;
+    
+    
+    int choice; 
+    do {
+    
+        cout << "Enter Your Choice: ";
+        cin >> choice;
+        cin.ignore();
+        
+        
+        switch(choice) {
+            case 1:
+                cashier();
+                break;
+            case 2:
+                invmenu();
+                break;
+            case 3:
+                reports();
+                break;
+            case 4:
+                cout << "\nYou selected item 4" << endl;
+                break;
+            default:
+                cout << "\nPlease enter a number from 1 to 4" << endl;
+                break;
+        }
+        
+    } while(choice != 4);
+	
+	return 0;
 }
